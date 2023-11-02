d853f6c4c5df1f6f64a91a9c7598fcbb707ff855
Shreyans0102 <shreyudivu@gmail.com>
2023-11-01 23:11:01 -0400

    state input components

---
+++ src/stateInputComponents/StateInputAddButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class StateInputAddButton extends Component {
+    render() {
+      return <div>
+        <button className="border-2 border-black m-5 px-5"> Button </button>
+        </div>
+    }
+  }
+
+
+  export default StateInputAddButton;
